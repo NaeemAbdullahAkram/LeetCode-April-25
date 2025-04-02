e31b72b6bdd8bdd054257443f4f310a4025bef7f
Naeem Akram <153123217+NaeemAbdullahAkram@users.noreply.github.com>
2025-04-02 14:04:28 +0500

    Time: 45 ms (94.54%), Space: 54.2 MB (23.29%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,14 @@
+
+function cancellable(fn, args, t) {
+    let timeoutId;
+
+    // Schedule the execution of fn after t milliseconds
+    timeoutId = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    // Return the cancel function
+    return () => {
+        clearTimeout(timeoutId); // Cancels the scheduled execution
+    };
+}
